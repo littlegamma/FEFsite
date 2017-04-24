4c1f7d67ab4bb7fa2358a7f2fb8b8d11b61fd719
Gamma <littlegamma@users.noreply.github.com>
2017-04-23 22:51:25 -0700

    partials and links

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "FEFsite"
   end
 
   test "should get about" do
-    get pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | FEFsite"
   end
