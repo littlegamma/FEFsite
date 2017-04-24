91293f0f631cd5723bbf974329ddd5e2baf6997e
Gamma <littlegamma@users.noreply.github.com>
2017-04-23 23:07:59 -0700

    new user registration stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'pages#home'
   get '/about', to: 'pages#about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/register', to: 'users#new'
 end
